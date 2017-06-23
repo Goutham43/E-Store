58c56a2c7a1440784f3fe4f8def5e344f3b008af
Goutham43 <siddhunamani@gmail.com>
2017-06-23 15:47:32 -0400

    update comments

--- server/index.js
+++ server/index.js
@@ -13,9 +13,7 @@ wagner.invoke(require('./auth'), { app: app });
 app.use('/api/v1', require('./api')(wagner));
 
 // Serve up static HTML pages from the file system.
-// For instance, '/6-examples/hello-http.html' in
-// the browser will show the '../6-examples/hello-http.html'
-// file.
+
 app.use(express.static('../', { maxAge: 4 * 60 * 60 * 1000 /* 2hrs */ }));
 
 app.listen(3000);
